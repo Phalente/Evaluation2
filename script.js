ba0455aa2b85326132e8d33bb5f56992440b9153
Robyn Phalente <phalenterobyn@gmail.com>
2022-05-17 14:14:53 +0200

    Fonction rollDice

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-//Fonction pour lancer le Dé
-const faceDice = document.getElementById('dice')
-const rollDice = document.getElementById('rollDiceBtn')
+//Lancer le Dé
 
+//Déclaration de variable
+const dice = document.getElementById('dice')
+const rollDice = document.getElementById('rollDiceBtn')
+const diceFace = document.getElementById('diceFace')
 let scoreDice = ''
+
+
+//Déclaration des fonctions
 
 function lancerDe() {   
     scoreDice = Math.floor((Math.random() * 6)+ 1)
+    changeDiceFace()
 //Afficher le résultat
     console.log(scoreDice)
 }
 
-faceDice.addEventListener('click',lancerDe)
+function changeDiceFace() {
+    diceFace.src=`../images/dice-${scoreDice}.svg`
+}
+
+//Déclaration des événements sur le Dom
+dice.addEventListener('click',lancerDe)
 rollDice.addEventListener('click',lancerDe)
+
